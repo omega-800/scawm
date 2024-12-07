c1de594c9179f8d48ded1c9b07bb3889a9db04a5
omega-800 <gshevoroshkin@gmail.com>
2024-12-07 22:03:54 +0100

    fun?

--- modules/river.nix
+++ modules/river.nix
@@ -1,13 +1,19 @@
 { lib, config, ... }:
 let
   inherit (lib)
+    pipe
     mkIf
     mapAttrs'
     nameValuePair
-    mapAttrs
+    filterAttrs
+    concatLines
+    hasAttr
+    isAttrs
     optionalAttrs
     hasInfix
-    replaceStrings
+    imap
+    attrsToList
+    escape
     ;
   inherit (config.scawm)
     integrations
@@ -19,32 +25,50 @@ let
     mkIntegration
     ;
   cfg = integrations.river;
-  mkName = n: (if (hasInfix " " n) then n else "None ${n}");
-  mapVals = f: a: mapAttrs' (n: v: nameValuePair (mkName n) (f v)) a;
-  escape = s: replaceStrings [ "'" ] [ "\\'" ] s;
-  escape' = s: replaceStrings [ "'" "\"" ] [ "\\'" "\\\"" ] s;
+  mkKeys = n: (if (hasInfix " " n) then n else "None ${n}");
+  mapVals = f: a: mapAttrs' (n: v: nameValuePair (mkKeys n) (f v)) a;
+  escapeA = s: escape [ "'" ] s;
+  concatImapAttrsLines =
+    f: a:
+    pipe a [
+      attrsToList
+      (imap (i: v: f i v.name v.value))
+      concatLines
+    ];
 in
 {
   options.scawm.integrations.river = mkIntegration "river";
   config = mkIf cfg.enable {
-    wayland.windowManager.river.settings = {
-      declare-mode = modeNames;
-      map =
-        {
-          normal = (mapVals (v: v) defmode) // (mapVals (v: "enter-mode ${v.name}") modes);
-        }
-        // (mapAttrs' (
-          _: v:
-          nameValuePair v.name (
-            (optionalAttrs (v ? switch) (
-              mapVals (v: "spawn 'riverctl enter-mode normal && riverctl spawn \"${escape' v}\"'") v.switch
-            ))
-            // (optionalAttrs (v ? stay) (mapVals (v: "spawn '${escape v}'") v.stay))
-            // {
-              "None Escape" = "enter-mode normal";
-            }
-          )
-        ) modes);
+    wayland.windowManager.river = {
+      settings = {
+        declare-mode = modeNames;
+        map =
+          {
+            normal = (mapVals (v: v) defmode) // (mapVals (v: "enter-mode ${v.name}") modes);
+          }
+          // (mapAttrs' (
+            _: v:
+            nameValuePair v.name (
+              (optionalAttrs (v ? stay) (mapVals (v: "spawn '${escapeA v}'") v.stay))
+              // {
+                "None Escape" = "enter-mode normal";
+              }
+            )
+          ) modes);
+      };
+      extraConfig = concatImapAttrsLines (
+        i: _: v:
+        concatImapAttrsLines (
+          i': keys: action:
+          let
+            fun = "__scawm_river_${toString i}_${toString i'}";
+          in
+          ''
+            ${fun}() { riverctl enter-mode normal; ${action}; }
+            riverctl map ${v.name} ${mkKeys keys} spawn '${fun}'
+          ''
+        ) v.switch
+      ) (filterAttrs (_: v: (isAttrs v) && (hasAttr "switch" v)) modes);
     };
   };
 }
--- modules/sway.nix
+++ modules/sway.nix
@@ -24,7 +24,9 @@ in
   config = mkIf cfg.enable {
     wayland.windowManager.sway.config = {
       inherit modifier;
-      keybindings = (mapAttrs (_: v: "exec ${v}") (spcToPlus defmode)) // (mapAttrs (_: v: "mode ${v.name}") (spcToPlus modes));
+      keybindings =
+        (mapAttrs (_: v: "exec ${v}") (spcToPlus defmode))
+        // (mapAttrs (_: v: "mode ${v.name}") (spcToPlus modes));
       modes = mapAttrs' (
         _: v:
         nameValuePair v.name (
