2a55572c0ac18f901dd6167c184c0d6fba74a6df
omega-800 <gshevoroshkin@gmail.com>
2024-12-07 22:27:33 +0100

    bacc to the basiccs

--- modules/river.nix
+++ modules/river.nix
@@ -1,16 +1,17 @@
 { lib, config, ... }:
 let
   inherit (lib)
-    pipe
     mkIf
     mapAttrs'
     nameValuePair
+    optionalAttrs
+    hasInfix
+    replaceStrings
+    pipe
     filterAttrs
     concatLines
     hasAttr
     isAttrs
-    optionalAttrs
-    hasInfix
     imap
     attrsToList
     escape
@@ -28,15 +29,15 @@ let
   mkKeys = n: (if (hasInfix " " n) then n else "None ${n}");
   mkSpawn = s: "spawn '${escapeA s}'";
   mapVals = f: a: mapAttrs' (n: v: nameValuePair (mkKeys n) (f v)) a;
-  escapeA = s: escape [ "'" ] s;
-  concatImapAttrsLines =
-    f: a:
-    pipe a [
-      attrsToList
-      (imap (i: v: f i v.name v.value))
-      concatLines
-    ];
+  escapeA = s: replaceStrings [ "'" ] [ "'\\''" ] s;
 in
+# concatImapAttrsLines =
+#   f: a:
+#   pipe a [
+#     attrsToList
+#     (imap (i: v: f i v.name v.value))
+#     concatLines
+#   ];
 {
   options.scawm.integrations.river = mkIntegration "river";
   config = mkIf cfg.enable {
@@ -50,26 +51,29 @@ in
           // (mapAttrs' (
             _: v:
             nameValuePair v.name (
-              (optionalAttrs (v ? stay) (mapVals mkSpawn v.stay))
+              (optionalAttrs (v ? switch) (
+                mapVals (v: "spawn 'riverctl enter-mode normal && ${escapeA v}'") v.switch
+              ))
+              // (optionalAttrs (v ? stay) (mapVals mkSpawn v.stay))
               // {
                 "None Escape" = "enter-mode normal";
               }
             )
           ) modes);
       };
-      extraConfig = concatImapAttrsLines (
-        i: _: v:
-        concatImapAttrsLines (
-          i': keys: action:
-          let
-            fun = "__scawm_river_${toString i}_${toString i'}";
-          in
-          ''
-            ${fun}() { riverctl enter-mode normal; ${action}; }
-            riverctl map ${v.name} ${mkKeys keys} spawn '${fun}'
-          ''
-        ) v.switch
-      ) (filterAttrs (_: v: (isAttrs v) && (hasAttr "switch" v)) modes);
+      # extraConfig = concatImapAttrsLines (
+      #   i: _: v:
+      #   concatImapAttrsLines (
+      #     i': keys: action:
+      #     let
+      #       fun = "__scawm_river_${toString i}_${toString i'}";
+      #     in
+      #     ''
+      #       ${fun}() { riverctl enter-mode normal; ${action}; }
+      #       riverctl map ${v.name} ${mkKeys keys} spawn '${fun}'
+      #     ''
+      #   ) v.switch
+      # ) (filterAttrs (_: v: (isAttrs v) && (hasAttr "switch" v)) modes);
     };
   };
 }
